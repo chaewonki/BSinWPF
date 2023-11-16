49f8ffa3ab6c965934a7d025da51f8e87a8944ef
cwk <chaewk15@gmail.com>
2023-11-16 17:55:45 +0900

    초기 커밋.

---
+++ BSinWPF/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Python.Runtime;
+using System.Windows;
+
+namespace BSinWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Runtime.PythonDLL = "python311.dll";
+
+            PythonEngine.Initialize();
+
+            using (Py.GIL())
+            {
+                dynamic module = Py.Import("script");
+
+                dynamic headlines = module.scrape_news_headlines();
+
+                foreach (string headline in headlines)
+                {
+                    lstBox.Items.Add(headline);
+                }
+
+            }
+
+            PythonEngine.Shutdown();
+        }
+    }
+}
---
+++ BSinWPF/script.py
@@ -0,0 +1,23 @@
+﻿import requests
+from bs4 import BeautifulSoup
+
+def scrape_news_headlines():
+    
+    response = requests.get('https://finance.naver.com/news/mainnews.naver')
+
+    if response.status_code == 200:
+    
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        headlines = soup.find_all('dd', class_='articleSubject')
+        
+        my_list = []
+        
+        for headline in headlines:
+            my_list.append(headline.text.strip())
+        
+        return my_list
+            
+    else:
+        return "Error: {response.status_code}"
+
